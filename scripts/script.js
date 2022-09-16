181069b4217d3faf543f4ed5794eeda609fbc7e0
figgyhunter <leonardo.roi3@gmail.com>
2022-09-16 13:12:14 +0200

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+const logo = document.getElementsByClassName('nav__image-container__image')[0];
+const hamburgerMenu = document.getElementsByClassName('nav__hamburger-menu__hamburger')[0];
+const navCtrOne = document.getElementsByClassName('nav__container-one')[0];
+const navCtrTwo = document.getElementsByClassName('nav__container-two')[0];
+const navContainer = document.getElementsByClassName('container')[0];
+const body = document.body;
+
+logo.addEventListener("click",()=>{window.location='index.html';}) 
+hamburgerMenu.addEventListener("click",()=>{
+    navCtrOne.classList.toggle("nav__container-one--visible");
+    navCtrTwo.classList.toggle("nav__container-two--visible");
+    navContainer.classList.toggle("container--visible");
+    body.classList.toggle("body--prevent-scroll");
+})
